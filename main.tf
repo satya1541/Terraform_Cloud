06a1e5e85715af46ba61216dfc56e27ee6e9481b
SATYA_N_B <satyastudy24@gmail.com>
2024-08-09 22:30:39 +0530

    Cloud

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 2
+ami = "ami-03972092c42e8c0ca"
+instance_type = "t2.micro"
+tags = {
+Name = "Terraform_cloud"
+}
+}
